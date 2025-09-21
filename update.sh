343b69fba91e77f7f1e97216742a904d6c64e941
nashehannafii <nashehannafii@gmail.com>
2025-09-22 01:36:23 +0700

    testing

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Menambahkan semua perubahan ke staging area
 git add .
-git commit -m "Update navbar titles and paths for localization"
+
+# Melakukan commit dengan pesan yang diambil dari argumen pertama
+# Tanda kutip di sekitar "$1" penting untuk menangani pesan komit yang mengandung spasi
+git commit -m "$1"
+
+# Mendorong perubahan ke branch 'main' di remote 'origin'
 git push origin main
